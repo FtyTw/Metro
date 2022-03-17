501da605dd9a8d7270df70372b35ff39e246362e
Pavel Kirov <kirov@maguru.com>
2022-03-17 13:03:27 +0200

    main components comments for use

--- src/components/ImageBackgroundComponent.js
+++ src/components/ImageBackgroundComponent.js
@@ -2,6 +2,13 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { Spinner } from './Spinner';
 
+/*
+    @param{source} String will be used as primary source to load the poster image
+    @param{backdrop} String will be used in case if poster loading fails
+    @param{ImageStyle} Object with styles supported by Image/ImageBackground components of React-Native
+    @param{resizeMode} String property of resize mode setting that accepts by Image/ImageBackground components of React-Native
+    component has 100% width and height values, so must be wrapped in parent component with needed sizes
+*/
 export const ImageBackgroundComponent = ({
     source = '',
     children,
@@ -53,7 +60,7 @@ ImageBackgroundComponent.propTypes = {
 const StyledImageBackground = styled.ImageBackground.attrs(({ imageStyle }) => ({
     imageStyle,
 }))`
-	width: 100%;
-	height: 100%;
-	justify-content: flex-end;
+    width: 100%;
+    height: 100%;
+    justify-content: flex-end;
 `;
--- src/components/TextHandler.js
+++ src/components/TextHandler.js
@@ -3,6 +3,13 @@ import styled from 'styled-components';
 
 import { useAppTheme } from '../hooks';
 
+/*
+    @param{children/text} String that can be passed without specifying attrs, just by wrapping the component tags
+    @param{inverted} Boolean in case if true component will be dependent on current app theme mode (light/dark)
+    @param{uppercase} Boolean in case if true {text} will be uppercased
+    @param {containerStyles} String extra css styles for component
+    @param {light} Boolean in case if true/false text font-weight property will be changed to normal/bold
+*/
 export const TextHandler = ({
     children: text,
     size = 's',
